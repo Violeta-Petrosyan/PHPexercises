3f8a11464c47e3da4dcdfc4cab09867d535f4e53
Violeta-Petrosyan <violeta.petrosyan12@gmail.com>
2021-07-07 19:38:34 +0400

    second

--- index.php
+++ index.php
@@ -111,9 +111,11 @@ echo "</pre>";
 //10
 $data=array(5, 3, 1, 3, 8, 7, 4, 1, 1, 3);
 $dataCount = count($data);
-for ($i = 1, $max = $data[0]; $i < $dataCount; ++$i)
-    if ($data[$i] > $max)
+for ($i = 1, $max = $data[0]; $i < $dataCount; ++$i) {
+    if ($data[$i] > $max) {
         $max = $data[$i];
+    }
+}
 $beads = array_fill(0, $max * $dataCount, 0);
 for ($i = 0; $i < $dataCount; ++$i)
     for ($j = 0; $j < $data[$i]; ++$j)
